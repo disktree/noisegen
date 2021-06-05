342ab4cdd99fc2a33c2a9ab83d5e2260ed83e703
tong <tong@disktree.net>
2021-06-05 21:26:19 +0200

    Initial hack

---
+++ App.hx
@@ -0,0 +1,131 @@
+
+import js.Browser.document;
+import js.Browser.window;
+import js.html.CanvasElement;
+import js.html.CanvasRenderingContext2D;
+import js.html.audio.AnalyserNode;
+import js.html.audio.AudioContext;
+import js.html.audio.AudioBufferSourceNode;
+import js.html.audio.ScriptProcessorNode;
+import js.lib.Float32Array;
+import js.lib.Uint8Array;
+import om.audio.generator.Noise;
+
+var colorBg = "#000";
+var colorFg = "#fff";
+
+var audio : AudioContext;
+var noise : String;
+var analyser : AnalyserNode;
+var noises : Map<String,Dynamic>;
+
+var canvas : CanvasElement;
+var ctx : CanvasRenderingContext2D;
+var freqs : Uint8Array;
+var times : Uint8Array;
+var animationFrameId : Int;
+
+function update( time : Float ) {
+
+    animationFrameId = window.requestAnimationFrame( update );
+
+    analyser.getByteFrequencyData( freqs );
+    analyser.getByteTimeDomainData( times );
+
+    var v : Float, x : Float, y : Float;
+    var hw = canvas.width/2, hh = canvas.height/2;
+    ctx.clearRect( 0, 0, canvas.width, canvas.height );
+    ctx.beginPath();
+    for( i in 0...analyser.fftSize ) {
+        v = i * (Math.PI/2)/180;
+        x = Math.cos(v) * ( 100+ times[i] );
+        y = Math.sin(v) * ( 100+ times[i] );
+        ctx.lineTo( hw + x, hh + y );
+    }
+    ctx.stroke(); 
+}
+
+function handleWindowResize(e) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function initAudio() {
+
+    audio = new AudioContext();
+
+    var gain = audio.createGain();
+    gain.gain.value = 0.5;
+    gain.connect( audio.destination );
+
+    analyser = audio.createAnalyser();
+    //analyser.smoothingTimeConstant = SMOOTHING;
+    //analyser.fftSize = 1024;
+    //analyser.minDecibels = -140;
+    //analyser.maxDecibels = 0;
+    analyser.connect( gain );
+
+    freqs = new Uint8Array( analyser.frequencyBinCount );
+    times = new Uint8Array( analyser.frequencyBinCount );
+
+    var noiseBufferSize = 4096;
+
+    var whiteNoise = audio.createScriptProcessor( noiseBufferSize, 1, 1 );
+    whiteNoise.onaudioprocess = e -> {
+        Noise.generateWhiteNoise( e.outputBuffer.getChannelData(0), noiseBufferSize );
+    };
+
+    var brownNoise = audio.createScriptProcessor( noiseBufferSize, 1, 1 );
+    brownNoise.onaudioprocess = e -> {
+        Noise.generateBrownNoise( e.outputBuffer.getChannelData(0), noiseBufferSize );
+    };
+
+    var pinkNoise = audio.createScriptProcessor( noiseBufferSize, 1, 1 );
+    pinkNoise.onaudioprocess = e -> {
+        Noise.generatePinkNoise( e.outputBuffer.getChannelData(0), noiseBufferSize );
+    };
+
+    noises = [
+        'white' => whiteNoise,
+        'pink' => pinkNoise,
+        'brown' => brownNoise
+    ];
+}
+
+function main() {
+
+    window.onload = () -> {
+
+        canvas = cast document.getElementById( 'spectrum' );
+        canvas.style.backgroundColor = '#000';
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx = canvas.getContext2d();
+        ctx.fillStyle = colorBg;
+        ctx.strokeStyle = colorFg;
+
+        var noiseTypes = ['white','brown','pink'];
+        for( type in noiseTypes ) {
+            var e = document.getElementById( type );
+            e.style.textDecoration = 'line-through';
+            e.onclick = _ -> {
+                if( audio == null ) {
+                    initAudio();
+                }
+                if( animationFrameId == null ) {
+                    animationFrameId = window.requestAnimationFrame( update );
+                }
+                if( e.style.textDecoration == 'none' ) {
+                    e.style.textDecoration = 'line-through';
+                    noises.get( type ).disconnect();
+                } else {
+                    e.style.textDecoration = 'none';
+                    noises.get( type ).connect( analyser );
+                }
+            }
+        }
+
+        window.addEventListener( 'resize', handleWindowResize, false );
+    }
+}
